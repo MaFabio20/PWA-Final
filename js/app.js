92ceb2676ed30369e59fcdd2c6dfbc63339115fd
MaFabio20 <mahechafabio2010@gmail.com>
2025-12-11 13:42:29 -0500

    Registro de services worker

--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
+
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("sw.js")
+      .then(() => console.log("Service Worker registrado correctamente"))
+      .catch(err => console.error("Error al registrar SW:", err));
+  }
+
+
 // Install prompt
 let deferredPrompt;
 
