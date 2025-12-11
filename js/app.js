f8e9723ad6fbdb50d9c218e0262956f8c469cf61
MaFabio20 <mahechafabio2010@gmail.com>
2025-12-11 13:44:19 -0500

    registrar services

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 
   if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.register("sw.js")
+    navigator.serviceWorker.register("./services-worker.js")
       .then(() => console.log("Service Worker registrado correctamente"))
       .catch(err => console.error("Error al registrar SW:", err));
   }
