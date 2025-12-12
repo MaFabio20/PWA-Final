aaf11aae8e99d8e0907e94bf0518a68a8b1404d9
MaFabio20 <mahechafabio2010@gmail.com>
2025-12-12 11:48:18 -0500

    services worker

--- services-worker.js
+++ services-worker.js
@@ -12,7 +12,19 @@ const ASSETS = [
   "/manifest.json"
 ];
 
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS))
+      .catch(err => {
+        console.error("Error cacheando:", err);
+      })
+  );
 
+  self.skipWaiting();
+});
+
+/*
 // INSTALACIÓN no sirve pero el cache si
 self.addEventListener("install", (event) => {
   event.waitUntil(
@@ -21,7 +33,7 @@ self.addEventListener("install", (event) => {
   self.skipWaiting();
 });
 ;
-
+*/
 // ACTIVACIÓN
 self.addEventListener("activate", (event) => {
   event.waitUntil(self.clients.claim());
